7c3fc510b7c099f7a1fe51e88f46bdbe53b836c0
christopaulhfh <christopaulhfh@gmail.com>
2026-05-15 15:37:36 +0530

    initial commit

---
+++ js.js
@@ -0,0 +1,11 @@
+const menuBtn = document.getElementById("menu-btn");
+const navLinks = document.querySelector(".nav-links");
+
+menuBtn.addEventListener("click", () => {
+  navLinks.classList.toggle("show");
+});
+
+// Contact Form Alert
+const contactForm = document.getElementById("contactForm");
+
+
